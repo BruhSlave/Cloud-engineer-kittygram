af71bd9a89afb84d71df469de422d11151837f30
BruhSlave <braslpaf@gmail.com>
2025-11-22 15:13:18 +0300

    Set default value at DEBUG

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_DJANGO_KEY")
 
-DEBUG = os.getenv("DEBUG_BOOL")
+DEBUG = os.getenv("DEBUG_BOOL", default=False)
 
 ALLOWED_HOSTS = ["*"]
 
